c660e078e19bf527c5d4bcd91acabcfd5fc13850
JMWC <jmwc@JMWCs-MBP.lan>
2021-09-29 19:00:21 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Module 1: Set up Git Repo!'));
+app.get('/', (req, res) => res.send('New Commit: Module 5: Finalize Pipeline!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
